831ffb9234485d371bd59164c4adae0db48a136b
mepraveenofficial@gmail.com <mepraveenofficial@gmail.com>
2021-10-01 00:31:23 +0530

    getpost and createpost functionalities added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,23 @@
-export const getposts = (req, res) => {
-  res.send("This Works");
+import PostMessage from "../models/postMessage.js";
+
+export const getposts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newpost = new PostMessage(post);
+
+  try {
+    await newpost.save();
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getposts } from "../controllers/posts.js";
+import { getposts, createPost } from "../controllers/posts.js ";
 
 const router = express.Router();
 
 router.get("/", getposts);
+router.post("/", createPost);
 
 export default router;
