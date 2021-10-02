443ec68df39375233ba577ba2498221e28bfb5de
mepraveenofficial@gmail.com <mepraveenofficial@gmail.com>
2021-10-02 09:54:30 +0530

    post delete,update functionalities added

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import cors from "cors";
-import Mongoose from "mongoose";
+
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-app.use("/posts", postRoutes); // /posts enna url vanna posts.js ill pokum
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://praveen:Sreekrishna%401@cluster0.bp8wt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //mongo db atlas (url of database)
+app.use("/posts", postRoutes);
 
+const CONNECTION_URL =
+  "mongodb+srv://praveen:Sreekrishna@1@cluster0.bp8wt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-Mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true, //connect with database
-})
-  .then(
-    () => app.listen(PORT, () => console.log(`Server Running On Port ${PORT}`)) // database connect ayal sever listen cheyum
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message)); //DB connect ayillakill error print cheyum
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
